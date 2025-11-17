8c04c315df7f5d80741c4f662edcdebe654d36cb
Test <ticketvault.test@gmail.com>
2025-11-17 14:44:34 +0300

    feature: init angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  protected readonly title = signal('Hello TicketVault!');
+}
