c7f29520eba7f39bd449c230f5241599554ebe07
Test <ticketvault.test@gmail.com>
2026-01-23 16:18:30 +0300

    feature: test input

--- apps/mobile-test-app/src/app/core/app-config/app.config.ts
+++ apps/mobile-test-app/src/app/core/app-config/app.config.ts
@@ -1,17 +1,21 @@
-import { appRoutes } from '../app.routes';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode, provideAppInitializer } from '@angular/core';
+import { appRoutes } from '../app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideIonicAngular(),
+    provideIonicAngular({
+      mode: 'ios',
+      useSetInputAPI: true,
+      swipeBackEnabled: false,
+      scrollAssist: true,
+      scrollPadding: true,
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      appRoutes,
-    ),
+    provideRouter(appRoutes),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
